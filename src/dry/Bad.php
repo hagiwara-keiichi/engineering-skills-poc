534076ab04f65d8070e167aee10db0e68f415e12
hagiwara-keiichi <hagiwara-keiichi@dmm.com>
2021-01-11 02:33:49 +0900

    dry

---
+++ src/dry/Bad.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace myapp\dry;
+require_once "../../vendor/autoload.php";
+
+class Bad
+{
+    public function pcPreviewAction()
+    {
+        // 処理A
+        // pcPreview用処理
+        // 処理B
+    }
+
+    public function spPreviewAction()
+    {
+        // 処理A
+        // spPreview用処理
+        // 処理B
+    }
+}
---
+++ src/dry/Good.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace myapp\dry;
+require_once "../../vendor/autoload.php";
+
+class Good
+{
+    public function previewAction()
+    {
+        $this->processingA();
+        $request = $this->getRequest();
+        if ($request['pc']) {
+            $this->pcPreviewProcessing();
+        } elseif ($request['sp']) {
+            $this->spPreviewProcessing();
+        }
+        $this->processingB();
+    }
+
+    private function processingA()
+    {
+        // 処理A
+    }
+
+    private function processingB()
+    {
+        // 処理B
+    }
+
+    private function pcPreviewProcessing()
+    {
+        // PCプレビュー用処理
+    }
+
+    private function spPreviewProcessing()
+    {
+        // PCプレビュー用処理
+    }
+}
