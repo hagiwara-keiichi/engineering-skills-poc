66b4e8ba373a1f88b5a9cff784a76b57716580c2
hagiwara-keiichi <hagiwara-keiichi@dmm.com>
2021-02-22 21:03:35 +0900

    test unit_test

---
+++ src/test/unit_test/User.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace myapp\test\unit_test;
+
+/**
+ * Class User
+ * @package myapp\test\unit_test
+ */
+class User
+{
+    public $first_name;
+    public $surname;
+
+    /**
+     * @return string
+     */
+    public function getFullName(): string
+    {
+        return trim("$this->first_name $this->surname");
+    }
+}
